25a0e90bbd7992f4a5c0761eb16359801c943393
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-04 04:21:10 -0300

    :sparkles: feat: requisito 8 concluído

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT
+  art.artist_name AS artista,
+  alb.album_name AS album
+FROM
+  SpotifyClone.artists AS art
+  INNER JOIN SpotifyClone.albums AS alb ON alb.artist_id = art.artist_id
+WHERE
+  art.artist_name = 'Walter Phoenix'
+ORDER BY
+  album_name;
