38024580db44973bd86e9e364cbc0acc4e2d32ed
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-04 04:35:12 -0300

    :sparkles: feat: requisito 9 concluído

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT
+  COUNT(s.song_id) AS quantidade_musicas_no_historico
+FROM
+  SpotifyClone.users AS us
+  INNER JOIN SpotifyClone.plays_history AS s ON s.user_id = us.user_id
+WHERE
+  us.user_name = 'Bill'
+GROUP BY
+  us.user_id;
