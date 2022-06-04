d5752c8b454c417c976aa93ac84c2579512bde8c
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-04 02:34:05 -0300

    :sparkles: feat: requisito 4 concluído

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT
+  us.user_name AS usuario,
+  IF(
+    MAX(pl.date) > '2020-12-31',
+    'Usuário ativo',
+    'Usuário inativo'
+  ) AS condicao_usuario
+FROM
+  SpotifyClone.users AS us
+  INNER JOIN SpotifyClone.plays_history AS pl ON us.user_id = pl.user_id
+GROUP BY
+  us.user_name
+ORDER BY
+  us.user_name;
