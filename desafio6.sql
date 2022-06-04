ab7ed9ed95070fd0cdfbc8b28f41c72c81705951
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-06-04 03:48:37 -0300

    :sparkles: feat: requisito 6 concluído

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-  ROUND(MIN(pl.price), 2) AS faturamento_minimo,
-  ROUND(MAX(pl.price), 2) AS faturamento_maximo,
-  ROUND(AVG(pl.price), 2) AS faturamento_medio,
-  ROUND(SUM(pl.price), 2) AS faturamento_total
+  FORMAT(MIN(pl.price), 2) AS faturamento_minimo,
+  FORMAT(MAX(pl.price), 2) AS faturamento_maximo,
+  FORMAT(AVG(pl.price), 2) AS faturamento_medio,
+  FORMAT(SUM(pl.price), 2) AS faturamento_total
 FROM
   SpotifyClone.users AS us
   INNER JOIN SpotifyClone.subscription_plans as pl ON pl.plan_id = us.plan_id;
